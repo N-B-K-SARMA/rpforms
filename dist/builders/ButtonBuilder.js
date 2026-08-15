372bb59afe6bf9491a471d486f85b14611ad8bba
Sarma <skarthikeya286@gmail.com>
2026-08-16 00:44:37 +0530

    chore: untrack dist folder from version control

--- dist/builders/ButtonBuilder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ButtonBuilder = void 0;
-const discord_js_1 = require("discord.js");
-class ButtonBuilder extends discord_js_1.ButtonBuilder {
-    constructor() { super(); }
-}
-exports.ButtonBuilder = ButtonBuilder;
--- dist/builders/QuestionBuilder.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.QuestionBuilder = void 0;
-const discord_js_1 = require("discord.js");
-class QuestionBuilder extends discord_js_1.TextInputBuilder {
-    constructor() { super(); this.setStyle(discord_js_1.TextInputStyle.Paragraph); }
-}
-exports.QuestionBuilder = QuestionBuilder;
